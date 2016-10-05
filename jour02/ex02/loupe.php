6ed75ec469c8c6509f976fc43c2e9f85d9b23f9d
Mounir <mounir@MounLin>
2016-10-05 23:10:58 +0200

    ajout ex 02

--- jour02/ex02/loupe.php
+++ jour02/ex02/loupe.php
@@ -1,34 +1,62 @@
 #!/usr/bin/php
 <?php
-	/*if (argc != 2)
-		return ;*/
-	function remplace($value, $begin, $end)
+	function maj($value, $lim, $lim2)
 	{
-		
+		$begin = strpos($value, $lim);
+		if ($lim === 'title="')
+			$begin += 7;
+		if ($begin == FALSE)
+				return NULL;
+		$end = strpos($value, $lim2, $begin);
+		if ($end == FALSE)
+				return NULL;
+		$str = substr($value, $begin, $end - $begin);
+		$str = strtoupper($str);
+		$value = substr_replace($value, $str, $begin, $end - $begin);
+		return ($value);
 	}
-
-	$tab = file("test.html");
-	//substr_replace
-	//strtoupper
-	$b = FALSE;
-	foreach ($tab as $value)
+	function remplace($str, $begin, $end)
 	{
-		if ($b == FALSE)
-		{
-			print($i."\n");
-			$begin = strpos($value, "<a");
-			if ($end != FALSE)
-				$b = TRUE;
-		}
-		if ($b == TRUE)
-		{
-			$end = strpos($value, "</a>");
-			if ($end != FALSE)
-			{
-				//remplace();
-				$b = FALSE;
-			}
-		}
-		print($value);
+		$val = substr($str, $begin, $end - $begin);
+		//print($value);
+		$value = maj($val, ">", "<");
+		if ($value == NULL)
+			return ($str);
+		//print($value);
+		$str = substr_replace($str, $value, $begin, $end - $begin);
+		$val = substr($str, $begin, $end - $begin);
+		$value = maj($val, 'title="', '"');
+		if ($value == NULL)
+			return ($str);
+		//print($value);
+		$str = substr_replace($str, $value, $begin, $end - $begin);
+		return($str);
 	}
+	if ($argc != 2)
+		return ;
+	$str = file_get_contents($argv[1], FALSE, NULL, 0);
+	if ($str == NULL)
+		return ;
+	//print($str);
+	$i = 0;
+	$lgt = strlen($str);
+	while ($i < $lgt)
+	{
+		$begin = strpos($str, "<a", $i);
+		//print($begin."\n");
+		if ($begin == FALSE)
+				break;
+		$i = $begin;
+		$end = strpos($str, '</a>', $i);
+		//print($end);
+		if ($end == FALSE)
+				break;
+		$i = $end;
+		$str = remplace($str, $begin, $end + 1);
+		if ($nbr = strpos($str, ">", $begin) == FALSE)
+			break ;
+		if ($nbr > $end)
+			break ;
+	}
+	print($str);
 ?>
