df9f3bd3aa9d38674cd901462b74576f8f697677
Mounir <mounir92600@hotmail.fr>
2016-10-07 00:09:23 +0200

    maj ex06 correction

--- jour03/ex06/members_only.php
+++ jour03/ex06/members_only.php
@@ -3,21 +3,23 @@
 	$usr = $_SERVER['PHP_AUTH_USER'];
 	if ( $usr == "zaz" &&  $pw == "jaimelespetitsponeys")
 	{
-	}
-	else
-	{
-		header("HTTP/1.0 404 NOT FOUND");
 ?>
-<html><body>Cette zone est uniquement accessible aux membres du site</body></html>
+	<html><body>Bonjour Zaz<br />
 <?php
-		exit (0);
-	}
-?>
-<html><body>Bonjour Zaz<br />
-<?php
-$str = file_get_contents("../img/42.png");
-$img = base64_encode($str);
-echo '<img src="data:image/png;base64,'.$img.'"';
+	$str = file_get_contents("../img/42.png");
+	$img = base64_encode($str);
+	echo '<img src="data:image/png;base64,'.$img.'"';
 ?>
 
 </body></html>
+<?php
+	}
+	else
+	{
+		header("HTTP/1.0 401 Unauthorized");
+		header("WWW-Authenticate: Basic realm=''Espace Membres''");
+?>
+<html><body>Cette zone est accessible uniquement aux membres du site</body></html>
+<?php
+	}
+?>
