3fc91aec09d66902d4c41bdcf66b42a3df36b941
Mounir <mounir92600@hotmail.fr>
2016-10-06 23:29:47 +0200

    fin jour 03

--- jour03/ex06/members_only.php
+++ jour03/ex06/members_only.php
@@ -1,10 +1,23 @@
 <?php
-//	print_r($_SERVER);
-	$value = $_SERVER[$PHP_AUTH_USR];
-	echo $value;
-/*foreach ($_SERVER as $value)
+	$pw = $_SERVER['PHP_AUTH_PW'];
+	$usr = $_SERVER['PHP_AUTH_USER'];
+	if ( $usr == "zaz" &&  $pw == "jaimelespetitsponeys")
 	{
-		if ($value == "zaz")
-			echo $value."\n";
-	}*/
+	}
+	else
+	{
+		header("HTTP/1.0 404 NOT FOUND");
 ?>
+<html><body>Cette zone est uniquement accessible aux membres du site</body></html>
+<?php
+		exit (0);
+	}
+?>
+<html><body>Bonjour Zaz<br />
+<?php
+$str = file_get_contents("../img/42.png");
+$img = base64_encode($str);
+echo '<img src="data:image/png;base64,'.$img.'"';
+?>
+
+</body></html>
